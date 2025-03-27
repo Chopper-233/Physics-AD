44d11f635d4efbf273696a0c443917a600053771
gUYao2023 <162338800+gUYao2023@users.noreply.github.com>
2025-03-27 12:08:30 +0800

    Update vidllama_tester.sh

--- scripts/vidllama_tester.sh
+++ scripts/vidllama_tester.sh
@@ -2,4 +2,4 @@ cd ..
 export PYTHONPATH=$(pwd)/src/VideoLLaMA:$PYTHONPATH
 
 python -m src.VideoLLaMA.test --obj "hinge"
-python -m src.VideoLLaMA.evaluator
+python -m src.VideoLLaMA.evaluator --obj "hinge"
